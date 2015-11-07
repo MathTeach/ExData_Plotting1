da1cdcc1e684d36ba2127995ea6fd8a436f1318b
Ria Yambao <ryambao123@gmail.com>
2015-11-06 20:51:57 -0600

    Replace dev.copy() with png() in the R codes.

--- plot1.R
+++ plot1.R
@@ -1,5 +1,12 @@
-## Read a subset of the big text file. The 2880 rows after the row containing
-## "31/1/2007;23:59:00" is read in. Note there are 2*(24*60) = 2880 rows - 
+## This code assumes that the zipped file containing the data set is
+## downloaded to the working directory and extracted there. This process
+## creates the subdirectory /exdata_data_household_power_consumption/ which 
+## contains the big data file 'household_power_consumption.txt' used in 
+## this R code.
+
+## Read a subset of the big data file - all the rows for two dates: 2-1-2007
+## and 2-2-2007. The 2880 rows after the row containing "31/1/2007;23:59:00"
+## are read in. Note there are 2*(24*60) = 2880 rows - 
 ## 2 days, 24 hours in a day, 60 minutes per hour.
 
 text_file <- "./exdata_data_household_power_consumption/household_power_consumption.txt"
@@ -13,18 +20,20 @@ names(subset_file) <- c("date", "time", "global_active_power",
            "global_reactive_power", "voltage", "global_intensity",
            "sub_metering_1", "sub_metering_2", "sub_metering_3")
 
-## Create "datetime" column POSIXlt with format - YYYY-MM-DD HH:MM:SS.
+## Create "datetime" column, class POSIXlt, with format - YYYY-MM-DD HH:MM:SS.
 ## Convert "date" column into Date class - YYYY-MM-DD.
 ## Note: When datetime column is created, date and time are both character.
 subset_file$datetime <- strptime(paste(subset_file$date, subset_file$time, 
             sep = " "), format = "%d/%m/%Y %T" )
 subset_file$date <- as.Date(subset_file$date, "%d/%m/%Y")
 
+## Initialize a histogram to be written to a png file. 
+png(filename = "plot1.png")
+
 ## Create histogram for global active power and change title/x axis label.
 hist(subset_file$global_active_power, col = "red", main = "Global Active Power",
             xlab = "Global Active Power (kilowatts)")
 
-## Copy histogram to a PNG file. Close the PNG device.
-dev.copy(png, file = "plot1.png")
+## Close the PNG device.
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -1,55 +1,63 @@
-## Read a subset of the big text file. The 2880 rows after the row containing
-## "31/1/2007;23:59:00" is read in. Note there are 2*(24*60) = 2880 rows - 
+## This code assumes that the zipped file containing the data set is
+## downloaded to the working directory and extracted there. This process
+## creates the subdirectory /exdata_data_household_power_consumption/ which 
+## contains the big data file 'household_power_consumption.txt' used in 
+## this R code.
+
+## Read a subset of the big data file - all the rows for two dates: 2-1-2007
+## and 2-2-2007. The 2880 rows after the row containing "31/1/2007;23:59:00"
+## are read in. Note there are 2*(24*60) = 2880 rows - 
 ## 2 days, 24 hours in a day, 60 minutes per hour.
 
 text_file <- "./exdata_data_household_power_consumption/household_power_consumption.txt"
 subset_file <- read.table(text_file, header = FALSE, sep = ";", na.strings = "?",
-                          colClasses = c("character","character", "numeric", "numeric", "numeric",
-                                         "numeric", "numeric", "numeric", "numeric"), 
-                          skip = grep("31/1/2007;23:59:00", readLines(text_file)), nrows = 2880)
+           colClasses = c("character","character", "numeric", "numeric", "numeric",
+           "numeric", "numeric", "numeric", "numeric"), 
+           skip = grep("31/1/2007;23:59:00", readLines(text_file)), nrows = 2880)
 
 ## Rename the header to meaningful variable names
 names(subset_file) <- c("date", "time", "global_active_power",
-                        "global_reactive_power", "voltage", "global_intensity",
-                        "sub_metering_1", "sub_metering_2", "sub_metering_3")
+           "global_reactive_power", "voltage", "global_intensity",
+           "sub_metering_1", "sub_metering_2", "sub_metering_3")
 
 ## Create "datetime" column, class POSIXlt, with format - YYYY-MM-DD HH:MM:SS.
 ## Convert "date" column into Date class - YYYY-MM-DD.
 ## Note: When datetime column is created, date and time are both character.
 subset_file$datetime <- strptime(paste(subset_file$date, subset_file$time, 
-                                       sep = " "), format = "%d/%m/%Y %T" )
+           sep = " "), format = "%d/%m/%Y %T" )
 subset_file$date <- as.Date(subset_file$date, "%d/%m/%Y")
 
-## Set two plots per row with two rows and two columns.
-par(mfrow = c(2, 2), mar = c(5,4,1,2))
+## Initialize plots to be written to a png file. 
+png(filename = "plot4.png")
+
+## Set parameter mfrow so there will be 2 plots per row.
+par(mfrow = c(2, 2))
 
 ## Create a line plot of datetime vs global_active_power.
 with(subset_file, plot(datetime, global_active_power, type = "l", xlab = "",
-                       ylab = "Global Active Power"))
+           ylab = "Global Active Power"))
 
 ## Create a line plot of datetime vs voltage.
 with(subset_file, plot(datetime, voltage, type = "l", xlab = "datetime",
-                       ylab = "Voltage"))
+           ylab = "Voltage"))
 
 ## Create line plots of datetime vs sub_metering_1, sub_metering_2 and
 ## sub_metering_3. First line plot is created and the rest are super-imposed
 ## on the same axes.
 with(subset_file, plot(datetime, sub_metering_1, type = "l", xlab = "",
-                       ylab = "Energy sub metering"))
+           ylab = "Energy sub metering"))
 with(subset_file, lines(datetime, sub_metering_2, col = "red"))
 with(subset_file, lines(datetime, sub_metering_3, col = "blue"))
 
 ## Create a legend on the top right corner of the plot.
 legend("topright", lty = 1, col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2",
-                  "Sub_metering_3"), ncol = 1.25, cex = 0.75, y.intersp = 0.6,
-                  bty = "n")
+           legend = c("Sub_metering_1", "Sub_metering_2",
+           "Sub_metering_3"), bty = "n")
 
 ## Create a line plot of datetime vs Global reactive power.
 with(subset_file, plot(datetime, global_reactive_power, type = "l", xlab = "datetime",
-                       ylab = "Global_reactive_power"))
+           ylab = "Global_reactive_power"))
 
 
-## Copy plots to a PNG file. Close the PNG device.
-dev.copy(png, file = "plot4.png")
+## Close the PNG device.
 dev.off()
